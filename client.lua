ba733e91fe65d4604e18fcd924ab3e58fdfd5bb6
MausCD <69720291+MausCD@users.noreply.github.com>
2021-08-21 21:04:58 +0200

    Add files via upload
    bugfix

--- client.lua
+++ client.lua
@@ -5,15 +5,30 @@ local playerPed = PlayerPedId()
 local vehicle
 vehicle = GetVehiclePedIsIn(playerPed, false)
 local roll =    GetEntityRoll(playerPed)
-if(roll >= -10) then
+if(roll >= -7) then
     if(roll <= 10) then
-        EnableControlAction(playerPed,59,true)
-        EnableControlAction(playerPed,63,true)
-    else
-        DisableControlAction(playerPed, 59,false)
-        DisableControlAction(playerPed, 63,false)
-    end
+        if(roll >= 170) then
+            DisableControlAction(playerPed, 59,false)
+            DisableControlAction(playerPed, 63,false)
+        else
+            EnableControlAction(playerPed,59,true)
+            EnableControlAction(playerPed,63,true)
+        end
+elseif(roll <= -13) then
+    EnableControlAction(playerPed,59,true)
+    EnableControlAction(playerPed,63,true)
 else
     DisableControlAction(playerPed, 59,false)
     DisableControlAction(playerPed, 63,false)
-end end end)
+end end end end)
+
+
+RegisterCommand('checkroll', function() 
+    local playerPed = PlayerPedId()
+    local vehicle
+    vehicle = GetVehiclePedIsIn(playerPed, false)
+    local roll =    GetEntityRoll(playerPed)
+    TriggerEvent('pNotify:SendNotification', {
+        text = {roll}
+    })
+end,false)
